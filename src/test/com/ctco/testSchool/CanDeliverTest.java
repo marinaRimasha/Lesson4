bb7cc733550972c52ef65a37c46fdb037f3ad503
Marina Rimash <rimasa.marina@gmail.com>
2020-01-28 17:25:37 +0200

    lesson5

--- src/test/com/ctco/testSchool/CanDeliverTest.java
+++ src/test/com/ctco/testSchool/CanDeliverTest.java
@@ -1,4 +1,4 @@
-package com.ctco.testSchool.tests;
+package com.ctco.testSchool;
 
 import com.ctco.testSchool.Member;
 import com.ctco.testSchool.Story;
@@ -16,12 +16,12 @@ public class CanDeliverTest {
     public void canDeliverStoryWithDefaultVelocity() {
         Team team = new Team();
         Member developer1 = new Member(Member.type.DEV);
-        developer1.codinSkills = true;
+//        developer1.codinSkills = true;
         Member developer2 = new Member(Member.type.DEV);
-        developer2.codinSkills = true;
+//        developer2.codinSkills = true;
         Member tester1 = new Member(Member.type.TEST); //actually no value of adding this member as he doesn't code and won't be used in calculations
-        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
-        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
+//        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
+//        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
 
         team.addMember(developer1);
         team.addMember(developer2);
@@ -41,13 +41,13 @@ public class CanDeliverTest {
     public void canDeliverDifferentVelocitySeveralStories() {
         Team team = new Team();
         Member developer1 = new Member(Member.type.DEV);
-        developer1.codinSkills = true;
+//        developer1.codinSkills = true;
         developer1.velocity = 5;
         Member developer2 = new Member(Member.type.DEV);
-        developer2.codinSkills = true;
+//        developer2.codinSkills = true;
         Member tester1 = new Member(Member.type.TEST); //actually no value of adding this member as he doesn't code and won't be used in calculations
-        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
-        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
+//        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
+//        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
 
         team.addMember(developer1);
         team.addMember(developer2);
@@ -70,13 +70,13 @@ public class CanDeliverTest {
     public void canNotDeliver() {
         Team team = new Team();
         Member developer1 = new Member(Member.type.DEV);
-        developer1.codinSkills = true;
+//        developer1.codinSkills = true;
         Member developer2 = new Member(Member.type.DEV);
-        developer2.codinSkills = true;
+//        developer2.codinSkills = true;
 
         Member tester1 = new Member(Member.type.TEST); //actually no value of adding this member as he doesn't code and won't be used in calculations
-        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
-        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
+//        tester1.testingSkills = true; //he doesn't code and won't be used in calculations
+//        tester1.codinSkills = false; //to ignore this member out of calculation as he isn't a dev
 
         team.addMember(developer1);
         team.addMember(developer2);
---
+++ src/test/com/ctco/testSchool/ExerciseTest.java
@@ -0,0 +1,106 @@
+package com.ctco.testSchool;
+import com.ctco.testSchool.FirstTask;
+import com.ctco.testSchool.SecondTask;
+import com.ctco.testSchool.ThirdTask;
+import com.ctco.testSchool.FourthTask;
+import com.ctco.testSchool.FifthTask;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExerciseTest {
+
+    @Test
+    public void testHappyPath_FirstTask() {
+        String exp = FirstTask.concatenate(0, 2, "Test");
+        assertEquals(exp, "3Test");
+    }
+
+    @Test
+    public void testNegativeInt_FirstTask() {
+        String exp = FirstTask.concatenate(-1, 2, " ");
+        assertEquals(exp, "2 ");
+    }
+
+    @Test
+    public void testNegativeTest_FirstTask() {
+        String exp = FirstTask.concatenate(1, 2, "s tring");
+        assertNotEquals(exp, "1s tring");
+    }
+
+    @Test
+    public void testHappy_SecondTask() {
+       assertTrue(new SecondTask().isEven(2));
+    }
+
+    @Test
+    public void testNegative_SecondTask() {
+        assertFalse(new SecondTask().isEven(3));
+    }
+
+
+    @Test
+    public void testZero_SecondTask() {
+        assertTrue(new SecondTask().isEven(0));
+    }
+
+    @Test
+    public void test_ThirdTask() {
+        String [] arr = {"one", "two", "three"};
+        String word = "two";
+        assertEquals(1, new ThirdTask().getElementPosition(arr, word), "Incorrect index");
+    }
+
+    @Test
+    public void testNegative_ThirdTask() {
+        String [] arr = {""};
+        String word = "two";
+        assertEquals(-1, new ThirdTask().getElementPosition(arr, word), "returned an index, should return -1");
+    }
+
+    @Test
+    public void testNull_ThirdTask() {
+        String [] arr = {"one", "two", "three"};
+        String word = null;
+        assertEquals(-1, new ThirdTask().getElementPosition(arr, word), "returned an index, should return -1");
+    }
+
+    @Test
+    public void testFourthTask() {
+        double actual = new FourthTask(2,3).getPerimeter();
+        assertEquals(10.0, actual, "Incorrect value of perimeter");
+    }
+
+    @Test
+    public void testFourthTask_zero() {
+        double actual = new FourthTask(0,3).getPerimeter();
+        assertEquals(6, actual, "Incorrect value of perimeter");
+    }
+
+    @Test
+    public void testFourthTask_negInt() {
+        double actual = new FourthTask(-2,3).getPerimeter();
+        assertEquals(2, actual, "Incorrect value of perimeter");
+    }
+
+    @Test
+    public void testFifthTask() {
+        FourthTask act = new FourthTask(-2,3);
+        String actual = new FifthTask().isRectangle(act);
+        assertEquals(true, actual, "Incorrect value of perimeter");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
