22feca8698e2d7735c6e4a727ee910d278d02fe5
Marina Rimash <rimasa.marina@gmail.com>
2020-01-30 00:12:05 +0200

    Homework5-added some kind of test for specs

--- src/test/com/ctco/testSchool/BlackBoxTests.java
+++ src/test/com/ctco/testSchool/BlackBoxTests.java
@@ -57,24 +57,37 @@ public class BlackBoxTests {
 
     @Test
     public void canDeliverQuality_SeveralStoriesBiggerTeam() {
-        Team team = new Team();
+        Team team1 = new Team(); //will name every dev+tester as a separate team
         Member developer1 = new Member(Member.type.DEV);
         Member tester1 = new Member(Member.type.TEST);
+        Member developer2 = new Member(Member.type.DEV);
+        Member tester2 = new Member(Member.type.TEST);
 
-        team.addMember(developer1);
-        team.addMember(tester1);
+        team1.addMember(developer1);
+        team1.addMember(tester1);
+
+        Team team2 = new Team();//will name every dev+tester as a separate team
+        team2.addMember(developer2);
+        team2.addMember(tester2);
 
         Story story = new Story();
-        story.setStoryPoints(8);//doesn't bring any value to the test for now
-        story.setTestPoints(5);
+        story.setStoryPoints(8);
+        story.setTestPoints(2);
 
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
+        Story story2 = new Story();
+        story2.setStoryPoints(8);
+        story2.setTestPoints(5);
 
-        assertTrue(team.canDeliverQuality(), "Team  couldn't deliver when they should");//canDeliverQuality doesn't count developer delivery
+        team1.backlog = new ArrayList<>(); //creating list of backlog
+        team1.backlog.add(story); //adding our story to list
+
+        team2.backlog = new ArrayList<>();
+        team2.backlog.add(story2);
         // as for now we should user canDeliver() to verify developers able to do their work in sprint and candeliverquality to verify testers work
-        team.sprintDays -= valueOf(story.storyPoints); //count how many days left for testing
-        assertFalse(team.canDeliverQuality(), "Team  could deliver when they should not");//canDeliverQuality doesn't count developer delivery
+        team1.sprintDays -= valueOf(story.storyPoints); //count how many days left for testing for the first story
+        team2.sprintDays -= valueOf(story2.storyPoints); //count how many days left for testing for the second story
+        assertTrue(team1.canDeliverQuality(), "Team  could not deliver when they should ");//canDeliverQuality doesn't count developer delivery
+        assertFalse(team2.canDeliverQuality(), "Team could deliver when they should not");//canDeliverQuality doesn't count developer delivery
     }
 
 }
