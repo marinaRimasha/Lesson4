f94da8966af95ae674ed37c98cff7c5174bbbac8
Marina Rimash <rimasa.marina@gmail.com>
2020-02-06 00:16:49 +0200

    Homework with exception handling

--- src/main/java/com/ctco/testSchool/Team.java
+++ src/main/java/com/ctco/testSchool/Team.java
@@ -1,5 +1,6 @@
 package com.ctco.testSchool;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,12 +35,32 @@ public class Team {
     This method do not support cross-functional team members capable doing both.
      */
 
-    public boolean canDeliverQuality() {
-        return members.stream()
-                .filter(item -> item.testingSkills)
-                .mapToDouble(i->i.velocity*sprintDays)
-                .sum() >=  backlog.stream().mapToDouble(s->s.testPoints).sum();
-    }
+        public boolean canDeliverQuality() {
+        validateInputs();
+            for (Story story:backlog
+            ) { if ((story.storyPoints+story.testPoints)>sprintDays) return false;
+            }
+            return  (canDeliver() && canTest());
+        }
+
+        private boolean canTest() {
+            return members.stream()
+                    .filter(item -> item.testingSkills)
+                    .mapToDouble(i->i.velocity*sprintDays)
+                    .sum() >=  backlog.stream().mapToDouble(s->s.testPoints).sum();
+        }
+
+        private void validateInputs() {
+            if (sprintDays<2) throw new IllegalArgumentException("Sprint should be at least two days long");
+            members.stream()
+                    .filter(item -> item.velocity<=0)
+                    .findAny()
+                    .ifPresent(a -> { throw new IllegalArgumentException("Velocity should be positive"); });
+            members.stream()
+                    .filter(item -> item.velocity>1)
+                    .findAny()
+                    .ifPresent(a -> { throw new IllegalArgumentException("Velocity can't be more than 1"); });
+        }
 
     /*
     Return true if developers could do the development part, ignoring the fact that there could be not enough testers
@@ -51,5 +72,20 @@ public class Team {
                 .sum() >=  backlog.stream().mapToDouble(s->s.storyPoints).sum();
     }
 
+    public static int getPrimeNumberClosesTo(int n) {
+        for(int c=0,s=0,d,N=n;c!=2;s++)
+            for(c=d=1,n+=n<N?s:-s;d<n;)
+                if(n%++d<1)c++;
+                return n;
+    }
 
+    public static String getHelloWorldText() {
+        int hours = LocalDateTime.now().getHour();
+        switch (hours) {
+            case 8: case 9: case 10: case 11: return "Good morning world!";
+            case 12: case 13: case 14: case 15: case 16: return "Good day world!";
+            case 17: return "Hello world!";
+            default: return "Good night world!";
+        }
+    }
 }
--- src/test/com/ctco/testSchool/BlackBoxTests.java
+++ src/test/com/ctco/testSchool/BlackBoxTests.java
@@ -3,6 +3,7 @@ package com.ctco.testSchool;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import static java.lang.Integer.valueOf;
@@ -10,11 +11,185 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BlackBoxTests {
 
+
+    @Test
+    public void canDeliverQuality_NotEnoughTestingTime() { //can't deliver as testers will not have enough time, there is only 1 tester per story
+        Team team = new Team();
+        Member developer1 = new Member(Member.type.DEV);
+        Member developer2 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+        Member tester2 = new Member(Member.type.TEST);
+
+        developer1.velocity = 2.0;
+        tester2.velocity = 2.0;
+
+        team.addMember(developer1);
+        team.addMember(developer2);
+        team.addMember(tester1);
+        team.addMember(tester2);
+
+        Story story = new Story();
+        story.setStoryPoints(8); //for first developer, they work at the same time
+        story.setTestPoints(2); //
+
+        Story story2 = new Story();
+        story2.setStoryPoints(8); //for second developer, they work at the same time
+        story2.setTestPoints(6); //
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+        team.backlog.add(story2);
+
+        try {
+            assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+        } catch (IllegalArgumentException e) {
+            assertEquals("Velocity can't be more than 1", e.getMessage(), "Exception not caught");
+        }
+    }
+
+    @Test
+    public void canDeliverQuality_OneDevAbsent() { //Developer got sick, the other took it up, could deliver
+        Team team = new Team();
+
+        Member developer1 = new Member(Member.type.DEV);
+        Member developer2 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+        developer1.velocity = 0;
+
+        team.addMember(developer1);
+        team.addMember(developer2);
+        team.addMember(tester1);
+
+        Story story = new Story();
+        Story story2 = new Story();
+        story.setStoryPoints(2);//doesn't bring any value to the test for now
+        story.setTestPoints(1);
+        story2.setStoryPoints(4);
+        story2.setTestPoints(3);
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+
+        try {
+            assertEquals( true, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+        } catch (IllegalArgumentException e) {
+            assertEquals("Velocity should be positive", e.getMessage(), "Exception not caught");
+        }
+    }
+
+    @Test
+    public void canDeliverQuality_OneTesterAbsent() { //Tester got sick, the other took it up, could deliver
+        Team team = new Team();
+
+        Member developer1 = new Member(Member.type.DEV);
+        Member developer2 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+        Member tester2 = new Member(Member.type.TEST);
+        tester1.velocity = 0;
+
+        team.addMember(developer1);
+        team.addMember(developer2);
+        team.addMember(tester1);
+        team.addMember(tester2);
+
+        Story story = new Story();
+        Story story2 = new Story();
+        story.setStoryPoints(6);//doesn't bring any value to the test for now
+        story.setTestPoints(2);
+        story2.setStoryPoints(6);
+        story2.setTestPoints(2);
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+
+        try {
+            assertEquals(true, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+        } catch (IllegalArgumentException e) {
+            assertEquals("Velocity should be positive", e.getMessage(), "Exception not caught");
+        }
+    }
+
+
+
+    @Test
+    public void canDeliverQuality_ZeroDevVelocity() { //Developer got sick, spilled story
+        Team team = new Team();
+
+        Member developer1 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+        developer1.velocity = 0;
+        team.addMember(developer1);
+        team.addMember(tester1);
+
+        Story story = new Story();
+        story.setStoryPoints(8);//doesn't bring any value to the test for now
+        story.setTestPoints(2);
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+
+        try {
+            assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+        } catch (IllegalArgumentException e) {
+            assertEquals("Velocity should be positive", e.getMessage(), "Exception not caught");
+        }
+    }
+
+    @Test
+    public void canDeliverQuality_ForgotToSetSprintDays() { //forgot to set sprint days //
+        Team team = new Team();
+        team.sprintDays = 0;
+        Member developer1 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+
+        team.addMember(developer1);
+        team.addMember(tester1);
+
+        Story story = new Story();
+        story.setStoryPoints(2);//doesn't bring any value to the test for now
+        story.setTestPoints(1);
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+
+        try {
+            assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
+            fail("Check sprint is >2");
+
+        } catch (IllegalArgumentException e) {
+            assertEquals("Sprint should be at least two days long", e.getMessage(), "Exception not caught");
+        }
+    }
+
     @Test
     public void canDeliverQuality_HappyPath() { //8 devpoints, 2 testpoints, 10 days enough
         Team team = new Team();
         Member developer1 = new Member(Member.type.DEV);
         Member tester1 = new Member(Member.type.TEST);
+
+        team.addMember(developer1);
+        team.addMember(tester1);
+
+        Story story = new Story();
+        story.setStoryPoints(8);//doesn't bring any value to the test for now
+        story.setTestPoints(2);
+
+        team.backlog = new ArrayList<>(); //creating list of backlog
+        team.backlog.add(story); //adding our story to list
+
+        assertEquals(true, team.canDeliverQuality(), "Team  couldn't deliver when they should");
+    }
+
+    @Test
+    public void canDeliverQuality_LessVelocityforTester() { //8 devpoints, 2 testpoints, 10 days not enough as tester works halftime
+        Team team = new Team();
+        Member developer1 = new Member(Member.type.DEV);
+        Member tester1 = new Member(Member.type.TEST);
+        tester1.velocity = 0.5;
 
         team.addMember(developer1);
         team.addMember(tester1);
@@ -99,46 +274,16 @@ public class BlackBoxTests {
         assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
     }
 
-      @Test
-    public void canDeliverQuality_NotEnoughTestingTime() { //can't deliver as testers will not have enough time, there is only 1 tester per story
-        Team team = new Team();
-        Member developer1 = new Member(Member.type.DEV);
-        Member developer2 = new Member(Member.type.DEV);
-        Member tester1 = new Member(Member.type.TEST);
-        Member tester2 = new Member(Member.type.TEST);
-
-        developer1.velocity = 2.0;
-        tester2.velocity = 2.0;
-
-        team.addMember(developer1);
-        team.addMember(developer2);
-        team.addMember(tester1);
-        team.addMember(tester2);
-
-        Story story = new Story();
-        story.setStoryPoints(8); //for first developer, they work at the same time
-        story.setTestPoints(2); //
-
-        Story story2 = new Story();
-        story2.setStoryPoints(8); //for second developer, they work at the same time
-        story2.setTestPoints(6); //
-
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
-        team.backlog.add(story2);
-        assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
-    }
 
        @Test
-    public void canDeliverQuality_NotEnoughTimeForDevelopers() { //can't deliver as developers will not have enough time, there is only 1 developer per story
+    public void canDeliverQuality_NotEnoughTimeForDevelopers() { //can't deliver as developers will not have enough time, there is only 1 developer per story //check this one
         Team team = new Team();
         Member developer1 = new Member(Member.type.DEV);
         Member developer2 = new Member(Member.type.DEV);
         Member tester1 = new Member(Member.type.TEST);
         Member tester2 = new Member(Member.type.TEST);
 
-        developer1.velocity = 2.0;
-
+        developer2.velocity = 2.0;
         team.addMember(developer1);
         team.addMember(developer2);
         team.addMember(tester1);
@@ -155,7 +300,13 @@ public class BlackBoxTests {
         team.backlog = new ArrayList<>(); //creating list of backlog
         team.backlog.add(story); //adding our story to list
         team.backlog.add(story2);
-        assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+           try {
+               assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
+
+           } catch (IllegalArgumentException e) {
+               assertEquals("Velocity can't be more than 1", e.getMessage(), "Exception not caught");
+           }
     }
 
 
@@ -172,6 +323,7 @@ public class BlackBoxTests {
 
         team.backlog = new ArrayList<>(); //creating list of backlog
         team.backlog.add(story); //adding our story to list
+
         assertEquals( false, team.canDeliverQuality(), "Team  could deliver when they should not");
     }
 
@@ -192,99 +344,45 @@ public class BlackBoxTests {
         assertEquals( true, team.canDeliverQuality(), "Team  could deliver when they should not");
     }
 
+
     @Test
-    public void canDeliverQuality_ForgotToSetSprintDays() { //forgot to set sprint days
-        Team team = new Team();
-        team.sprintDays = 0;
-        Member developer1 = new Member(Member.type.DEV);
-        Member tester1 = new Member(Member.type.TEST);
-
-        team.addMember(developer1);
-        team.addMember(tester1);
-
-        Story story = new Story();
-        story.setStoryPoints(2);//doesn't bring any value to the test for now
-        story.setTestPoints(1);
-
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
-
-        assertEquals(false, team.canDeliverQuality(), "Team  couldn't deliver when they should");
+    public void Test1() {
+        assertEquals(3,  Team.getPrimeNumberClosesTo(3), "");
     }
 
     @Test
-    public void canDeliverQuality_ZeroDevVelocity() { //Developer got sick, spilled story
-        Team team = new Team();
-
-        Member developer1 = new Member(Member.type.DEV);
-        Member tester1 = new Member(Member.type.TEST);
-        developer1.velocity = 0;
-        team.addMember(developer1);
-        team.addMember(tester1);
-
-        Story story = new Story();
-        story.setStoryPoints(8);//doesn't bring any value to the test for now
-        story.setTestPoints(2);
-
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
-
-        assertEquals(false, team.canDeliverQuality(), "Team  couldn't deliver when they should");
+    public void Test2() {
+        assertEquals(7,  Team.getPrimeNumberClosesTo(8), "returned incorrect number");
     }
 
     @Test
-    public void canDeliverQuality_OneDevAbsent() { //Developer got sick, the other took it up, could deliver
-        Team team = new Team();
-
-        Member developer1 = new Member(Member.type.DEV);
-        Member developer2 = new Member(Member.type.DEV);
-        Member tester1 = new Member(Member.type.TEST);
-        developer1.velocity = 0;
-
-        team.addMember(developer1);
-        team.addMember(developer2);
-        team.addMember(tester1);
-
-        Story story = new Story();
-        Story story2 = new Story();
-        story.setStoryPoints(2);//doesn't bring any value to the test for now
-        story.setTestPoints(1);
-        story2.setStoryPoints(4);
-        story2.setTestPoints(3);
-
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
-
-        assertEquals(true, team.canDeliverQuality(), "Team  couldn't deliver when they should");
+    public void Test3() {
+        int result = Team.getPrimeNumberClosesTo(11);
+//        if ((result == 7 )|| (result == 11 )) {
+//            assertTrue(true);
+//        } else {
+//            fail("Returned "+result+" expected was 7 or 11");
+//        }
+        assertTrue(((result == 7 )|| (result == 11 )), "Returned "+result+" expected was 7 or 11");
     }
+
+//    @Test
+//    public void TestHelloWorld() {
+//        assertEquals("Hello world!",  Team.getHelloWorldText(), "Didnt return Hello World as it is 17 now");
+//    }
 
     @Test
-    public void canDeliverQuality_OneTesterAbsent() { //Tester got sick, the other took it up, could deliver
-        Team team = new Team();
-
-        Member developer1 = new Member(Member.type.DEV);
-        Member developer2 = new Member(Member.type.DEV);
-        Member tester1 = new Member(Member.type.TEST);
-        Member tester2 = new Member(Member.type.TEST);
-        tester1.velocity = 0;
-
-        team.addMember(developer1);
-        team.addMember(developer2);
-        team.addMember(tester1);
-        team.addMember(tester2);
-
-        Story story = new Story();
-        Story story2 = new Story();
-        story.setStoryPoints(6);//doesn't bring any value to the test for now
-        story.setTestPoints(2);
-        story2.setStoryPoints(6);
-        story2.setTestPoints(2);
-
-        team.backlog = new ArrayList<>(); //creating list of backlog
-        team.backlog.add(story); //adding our story to list
-
-        assertEquals(true, team.canDeliverQuality(), "Team  couldn't deliver when they should");
+    public void TestDayPhase() {
+        int localHour = LocalDateTime.now().getHour();
+        if ( (localHour == 8 )||  (localHour == 9) || (localHour == 10 ) ||(localHour == 11 )) {
+           assertEquals("Good morning world!",  Team.getHelloWorldText(), "Incorrect message: "+Team.getHelloWorldText());
+       } else if ( (localHour == 12 )||  (localHour == 13) || (localHour == 14 ) ||(localHour == 15 ) || (localHour == 16 )) {
+            assertEquals("Good day world!",  Team.getHelloWorldText(), "Incorrect message: "+Team.getHelloWorldText());
+        } else if (localHour == 17 ) {
+            assertEquals("Hello world!",  Team.getHelloWorldText(), "Incorrect message: "+Team.getHelloWorldText());
+        } else {
+            assertEquals("Good night world!",  Team.getHelloWorldText(), "Incorrect message: "+Team.getHelloWorldText());
+        }
     }
-
 
 }
